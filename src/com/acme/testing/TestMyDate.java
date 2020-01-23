b00004303565f59929326fa815874b88949d1c3b
don spire <don.spire1@gmail.com>
2020-01-23 13:11:27 -0800

    updated TestMyDate

--- src/com/acme/testing/TestMyDate.java
+++ src/com/acme/testing/TestMyDate.java
@@ -1,5 +1,10 @@
 package com.acme.testing;
 import com.acme.utils.MyDate;
+import com.acme.domain.Order;
+import com.acme.domain.Service;
+import com.acme.domain.Solid;
+import com.acme.utils.MyDate;
+import com.acme.domain.Good.UnitOfMeasureType;
 
 public class TestMyDate{
 
@@ -12,7 +17,7 @@ public class TestMyDate{
 		date2.setYear(1918);
 
 		MyDate date3 = new MyDate();
-		date3.setDate(4,21,1968);
+		date3.setDate(1,1,2016);
 		date3.setDay(28);
 		System.out.println("This is date3 " + date3);
 
@@ -35,11 +40,22 @@ public class TestMyDate{
 		
 		MyDate newYear = new MyDate(1,1,2009);
 		MyDate fiscalStart = new MyDate(1,1,2009);
+		//new test case for equals about type casting
+		Solid s2 = new Solid("Acme Balloon", 1401, 15, UnitOfMeasureType.CUBIC_FEET, false, 10, 5, 5);
+		Order balloons = new Order(date2, 1000.00, "Bugs Bunny", s2, 125);
 		
 		if (newYear.equals(fiscalStart))
 			System.out.println("These two dates are equal");
 		else System.out.println("These two dates are not equal");
 		
+		if (newYear.equals(s2))
+			System.out.println("These two dates are equal");
+		else System.out.println("These two dates are not equal");
+		
+		MyDate.listHolidays();
+		MyDate acme101 = new MyDate(1,1,2016);
+		Solid s3 = new Solid("Acme Balloon", 1401, 15, UnitOfMeasureType.CUBIC_FEET, false, 10, 5, 5);
+		Order balloons101 = new Order(acme101, 1499, "Porky Pig", s3, 125);
 		
 	}
 }
