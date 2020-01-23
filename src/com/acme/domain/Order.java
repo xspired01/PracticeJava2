eea91b122fdcc2cb728a898c2d0789a9fcc81587
don spire <don.spire1@gmail.com>
2020-01-23 13:11:59 -0800

    updated Order

--- src/com/acme/domain/Order.java
+++ src/com/acme/domain/Order.java
@@ -3,11 +3,75 @@ package com.acme.domain;
 import com.acme.utils.MyDate;
 
 public class Order {
-	MyDate orderDate;
-	double orderAmount = 0.00;
-	String customer;
-	private Good product;
-	int quantity;
+	private MyDate orderDate;
+	private double orderAmount = 0.00;
+	private String customer;
+	private Product product;
+	private int quantity;
+	private static Rushable rushable;
+	
+	public static Rushable getRushable()
+	{
+		return rushable;
+	}
+	public static void setRushable(Rushable rushable)
+	{
+		Order.rushable = rushable;
+	}
+	public boolean isPriorityOrder()
+	{
+		boolean priorityOrder = false;
+		if (rushable != null) {
+			priorityOrder = rushable.isRushable(orderDate,  orderAmount);
+		}
+		return priorityOrder;
+	}
+
+	public MyDate getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(MyDate orderDate) {
+		if (isHoliday(orderDate)) {
+			System.out.println("Order date, " + orderDate + ", cannot be set to a holiday!");
+		} else {
+			this.orderDate = orderDate;
+		}
+		
+	}
+
+	public double getOrderAmount() {
+		return orderAmount;
+	}
+
+	public void setOrderAmount(double orderAmount) {
+		if (orderAmount > 0) {
+			this.orderAmount = orderAmount;
+		} else {
+			System.out.println("Attempting to set the orderAmount to a value less than or equal to zero");
+		}
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+	// default constructor
+//		public Order() {
+//			
+//		};
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 
 	public static double taxRate;
 	// initialization block for taxRate
@@ -28,31 +92,33 @@ public class Order {
 		return orderAmount * Order.taxRate;
 	}
 
-	
 	public char jobSize() {
-		
+
 		if (quantity <= 25) {
 			return 'S';
 		} else if (quantity < 75) {
 			return 'M';
 		} else if (quantity < 150) {
 			return 'L';
-		} else  {
+		} else {
 			return 'X';
 		}
 	}
-	
+
 	public double computeTotal() {
 		double orderTotal = orderAmount;
-		//Order total = order amount - discount + tax
-		
+		// Order total = order amount - discount + tax
+
 		switch (jobSize()) {
-		case 'M': orderTotal = orderTotal - (orderAmount * 0.01);
-		break;
-		case 'L': orderTotal = orderTotal - (orderAmount * 0.02);
-		break;
-		case 'X': orderTotal = orderTotal - (orderAmount * 0.03);
-		break;
+		case 'M':
+			orderTotal = orderTotal - (orderAmount * 0.01);
+			break;
+		case 'L':
+			orderTotal = orderTotal - (orderAmount * 0.02);
+			break;
+		case 'X':
+			orderTotal = orderTotal - (orderAmount * 0.03);
+			break;
 		}
 		if (orderAmount <= 1500) {
 			orderTotal = orderTotal + computeTax();
@@ -60,17 +126,20 @@ public class Order {
 		return orderTotal;
 	}
 
-	//default constructor
-	public Order() {
-		
-	};
-	
-	public Good getProduct() {
-		return product;
+	public int getQuantity() {
+		return quantity;
 	}
 
-	public Order(MyDate d, double amt, String c, Good p, int q) {
-		orderDate = d;
+	public void setQuantity(int quantity) {
+		if (quantity > 0) {
+			this.quantity = quantity;
+		} else {
+			System.out.println("Attempting to set the quantity to a value less than or equal to zero.");
+		}
+	}
+
+	public Order(MyDate d, double amt, String c, Product p, int q) {
+		setOrderDate(d);
 		orderAmount = amt;
 		customer = c;
 		product = p;
@@ -89,5 +158,15 @@ public class Order {
 
 	public String toString() {
 		return quantity + " ea. " + product + " for " + customer;
+	}
+	
+	private boolean isHoliday(MyDate proposedDate) {
+		boolean result = false;
+		for (MyDate holiday : MyDate.getHolidays()) {
+			if(holiday.equals(proposedDate)) {
+				result = true;
+			}
+		}
+		return result;
 	}
 }
